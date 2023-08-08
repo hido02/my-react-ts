8532a5c3b84af38060bde7c7d1c507ba8538db21
hido02 <91791632+hido02@users.noreply.github.com>
2023-08-09 01:01:53 +0900

    feat: map과 GenericList

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { useState } from 'react';
 import { FCCounter } from './Counter';
+import { GenericList } from './GenericList';
 import './style.css';
 
 const App: React.FC = () => {
@@ -9,10 +10,17 @@ const App: React.FC = () => {
     setCount(prevCount => prevCount + 1);
   };
 
+  const items = ["Item 1", "Item 2", "Item 3"];
+
+  const itemRenderer = (item: string) => {
+    return <div key={item}>{item}</div>;
+  };
+
   return (
     <div>
       <h1>My App</h1>
       <FCCounter label="Count" count={count} onIncrement={onIncrement} />
+      <GenericList items={items} itemRenderer={itemRenderer} />
     </div>
   );
 };
